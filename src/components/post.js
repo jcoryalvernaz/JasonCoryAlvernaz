355ee3a194d452d9e889f499fb3a4340d422a895
Jason Alvernaz <jason.alvernaz@lcb.state.nv.us>
2019-07-30 11:16:05 -0700

    first commit

---
+++ src/components/post.js
@@ -0,0 +1,31 @@
+import React from "react"
+import { graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+export default function Post({ data }) {
+  const { markdownRemark: post } = data
+
+  return (
+    <Layout>
+      <SEO title={post.frontmatter.title} />
+      <div>
+        <h1>{post.frontmatter.title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: post.html }} />
+      </div>
+    </Layout>
+  )
+}
+
+export const postQuery = graphql`
+  query BlogPostByPath($path: String!) {
+    markdownRemark(frontmatter: { path: { eq: $path } }) {
+      html
+      frontmatter {
+        path
+        title
+      }
+    }
+  }
+`
