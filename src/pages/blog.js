6c69c4224e265015d343afb245ff5876f14b0abf
Jason Alvernaz <jason.alvernaz@lcb.state.nv.us>
2019-08-08 15:10:05 -0700

    initial styling pass for blog list page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,9 +1,44 @@
 import React from "react"
 import { Link } from "gatsby"
 import Img from "gatsby-image"
+import styled from "styled-components"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+
+const BlogList = styled.ul`
+  padding: 0;
+  list-style: none;
+  li {
+    display: grid;
+    grid-gap: 1rem;
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: 1fr 1fr;
+    border-top: 0.5rem solid ${props => props.theme.green};
+    padding-top: 2rem;
+    padding-bottom: 2rem;
+  }
+  div {
+    grid-column: 1 / span 1;
+    grid-row: 1 / span 2;
+  }
+  a {
+    color: ${props => props.theme.purple};
+    font-size: 2.5rem;
+  }
+  @media (max-width: 500px) {
+    li {
+      grid-template-columns: 1fr;
+      grid-template-rows: 3fr 1fr;
+    }
+    div {
+      grid-row: 1 / span 1;
+    }
+    a {
+      grid-row: span 1 / -1;
+    }
+  }
+`
 
 const BlogPage = ({ data }) => {
   const posts = [...data.allMarkdownRemark.edges]
@@ -12,22 +47,22 @@ const BlogPage = ({ data }) => {
       <SEO title="Blog" />
       <h1>Hi from the blog page</h1>
       <p>Welcome to page 3</p>
-      <ul>
+      <BlogList>
         {posts.map(post => {
           return (
-            <li>
+            <li key={post.node.id}>
               <Img
                 fluid={
                   post.node.frontmatter.featuredImage.childImageSharp.fluid
                 }
               />
-              <Link key={post.node.id} to={post.node.frontmatter.path}>
+              <Link to={post.node.frontmatter.path}>
                 {post.node.frontmatter.title}
               </Link>
             </li>
           )
         })}
-      </ul>
+      </BlogList>
       <Link to="/">Go back to the homepage</Link>
     </Layout>
   )
