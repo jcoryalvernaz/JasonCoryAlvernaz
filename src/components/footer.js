8fe1c87ca23035180a1e0c0d251079231af99028
Jason Alvernaz <jason.alvernaz@lcb.state.nv.us>
2019-08-09 15:40:54 -0700

    styling footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,25 +1,35 @@
 import React from "react"
 import styled from "styled-components"
 
+import hexagons from "../images/hexagons.svg"
+import heart from "../images/heart.svg"
+
 const StyledFooter = styled.footer`
+  padding: 2rem;
   background: ${props => props.theme.purple};
+  background-image: url(${hexagons});
   color: ${props => props.theme.white};
   display: grid;
-  grid-template-columns: 1fr 1fr;
+  grid-template-columns: 1fr;
   border-top: 2px solid ${props => props.theme.black};
-  align-items: center;
+  justify-items: center;
   a {
     color: ${props => props.theme.white};
   }
-  p {
-    justify-self: end;
+  .heart {
+    margin-bottom: -0.2rem;
+    margin-left: 0.2rem;
+    margin-right: 0.2rem;
+    height: 1.5rem;
   }
 `
 
 const Footer = () => (
   <StyledFooter>
     © {new Date().getFullYear()}, Jason Cory Alvernaz
-    <p>Made With love in Reno</p>
+    <p>
+      Made With <img className="heart" src={heart} alt="love heart" /> in Reno
+    </p>
   </StyledFooter>
 )
 
