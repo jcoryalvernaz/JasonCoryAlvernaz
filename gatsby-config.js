1c785cf0d68344a5430b8950599c248dbc24f37f
Jason Cory Alvernaz <alvernazjason@gmail.com>
2019-08-09 20:42:45 -0700

    build fixes

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,7 +44,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#4c2a85`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/JasonCoryAlvernaz.jpg`, // This path is relative to the root of the site.
       },
     },
     {
