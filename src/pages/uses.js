8c68ac086c113caceaf48d3c3d549dd7b77b6e32
Jason Alvernaz <jason.alvernaz@lcb.state.nv.us>
2019-08-12 12:41:48 -0700

    add initial /uses page

---
+++ src/pages/uses.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import HeadingStyles from "../styles/HeadingStyles"
+import ParagraphStyles from "../styles/ParagraphStyles"
+
+const UsesPage = () => (
+  <Layout>
+    <SEO title="Uses" />
+    <HeadingStyles>Hello!</HeadingStyles>
+    <ParagraphStyles>Welcome to my /uses page!</ParagraphStyles>
+  </Layout>
+)
+
+export default UsesPage
