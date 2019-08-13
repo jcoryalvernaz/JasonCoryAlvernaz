28ce8bb70354ab5ab4cf974b6948ebba316b9984
Jason Alvernaz <jason.alvernaz@lcb.state.nv.us>
2019-08-13 11:25:31 -0700

    adding social to thanks page

--- src/pages/thanks.js
+++ src/pages/thanks.js
@@ -2,6 +2,7 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Social from "../components/social"
 import HeadingStyles from "../styles/HeadingStyles"
 import ParagraphStyles from "../styles/ParagraphStyles"
 
@@ -13,6 +14,7 @@ const ThanksPage = () => (
       I will reply to your inquiry soon. In the meantime, make sure to check me
       out on social media by visiting the links below.
     </ParagraphStyles>
+    <Social></Social>
   </Layout>
 )
 
