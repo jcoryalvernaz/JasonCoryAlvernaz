268dfe84e0a9b686d4230ddd7c4a56f9a9f50d0f
Jason Alvernaz <jason.alvernaz@lcb.state.nv.us>
2019-08-13 11:12:12 -0700

    editing 404 page content

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,12 +2,20 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import HeadingStyles from "../styles/HeadingStyles"
+import ParagraphStyles from "../styles/ParagraphStyles"
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <HeadingStyles>404: Not Found</HeadingStyles>
+    <ParagraphStyles>
+      You just hit a page that doesn&#39;t exist... Oh no.
+    </ParagraphStyles>
+    <ParagraphStyles>
+      Don't worry, there are plenty of other pages to visit. Head up to the
+      navigation and take a look around.
+    </ParagraphStyles>
   </Layout>
 )
 
