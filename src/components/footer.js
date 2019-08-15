db482404797c5e13c6ae4353a0b48d3618c5772e
Jason Alvernaz <jason.alvernaz@lcb.state.nv.us>
2019-08-15 09:45:18 -0700

    aria-label for switch

--- src/components/footer.js
+++ src/components/footer.js
@@ -32,6 +32,7 @@ const Footer = ({ toggleTheme, isChecked }) => (
     <br />
     <ToggleStyles>
       <input
+        aria-label="Change Site Theme"
         className="switch"
         type="checkbox"
         onChange={toggleTheme}
