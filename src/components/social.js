342a691b573c4caf5b084ec4db28fae9ee46ea68
Jason Alvernaz <jason.alvernaz@lcb.state.nv.us>
2019-08-15 15:33:32 -0700

    inital arrow animation

--- src/components/social.js
+++ src/components/social.js
@@ -2,7 +2,7 @@ import React, { useState } from "react"
 import styled from "styled-components"
 
 const SocialWrapper = styled.div`
-  padding-top: 20vmin;
+  padding-top: 10vmin;
   padding-bottom: 20vmin;
   display: grid;
   grid-gap: 2rem;
@@ -11,16 +11,39 @@ const SocialWrapper = styled.div`
   justify-items: center;
   h2 {
     grid-column: 1 / -1;
+    margin-bottom: 0;
   }
   .arrow {
-    grid-column: 1 / -1;
     fill: ${props => props.theme.purple};
-    height: 3rem;
-    width: 3rem;
+    height: 4rem;
+    width: 4rem;
     transform: rotate(90deg);
   }
   @media (max-width: 630px) {
     grid-template-columns: 1fr;
+  }
+`
+
+const AnimationStyles = styled.div`
+  grid-column: 1 / -1;
+  animation-name: bounce;
+  animation-duration: 1.8s;
+  animation-timing-function: cubic-bezier(0.175, 0.885, 0.32, 1.275);
+  animation-iteration-count: 3;
+  @keyframes bounce {
+    0%,
+    20%,
+    50%,
+    80%,
+    100% {
+      transform: translateY(0px);
+    }
+    40% {
+      transform: translateY(-15px);
+    }
+    60% {
+      transform: translateY(-5px);
+    }
   }
 `
 
@@ -50,20 +73,22 @@ const Social = () => {
   return (
     <SocialWrapper>
       <h2>Let's Connect</h2>
-      <svg
-        className="arrow"
-        xmlns="http://www.w3.org/2000/svg"
-        version="1.1"
-        x="0px"
-        y="0px"
-        viewBox="0 0 100 100"
-      >
-        <g>
+      <AnimationStyles>
+        <svg
+          className="arrow"
+          xmlns="http://www.w3.org/2000/svg"
+          version="1.1"
+          x="0px"
+          y="0px"
+          viewBox="0 0 100 100"
+        >
           <g>
-            <path d="M95.9,46.2L65.4,15.7c-2.1-2.1-5.5-2.1-7.5,0c-2.1,2.1-2.1,5.5,0,7.5l21.5,21.5H7.8c-2.9,0-5.3,2.4-5.3,5.3    c0,2.9,2.4,5.3,5.3,5.3h71.5L57.9,76.8c-2.1,2.1-2.1,5.5,0,7.5c1,1,2.4,1.6,3.8,1.6s2.7-0.5,3.8-1.6l30.6-30.6    c1-1,1.6-2.4,1.6-3.8C97.5,48.6,96.9,47.2,95.9,46.2z"></path>
+            <g>
+              <path d="M95.9,46.2L65.4,15.7c-2.1-2.1-5.5-2.1-7.5,0c-2.1,2.1-2.1,5.5,0,7.5l21.5,21.5H7.8c-2.9,0-5.3,2.4-5.3,5.3    c0,2.9,2.4,5.3,5.3,5.3h71.5L57.9,76.8c-2.1,2.1-2.1,5.5,0,7.5c1,1,2.4,1.6,3.8,1.6s2.7-0.5,3.8-1.6l30.6-30.6    c1-1,1.6-2.4,1.6-3.8C97.5,48.6,96.9,47.2,95.9,46.2z"></path>
+            </g>
           </g>
-        </g>
-      </svg>
+        </svg>
+      </AnimationStyles>
       {links.map((link, i) => {
         return (
           <a key={i} target="_blank" rel="noopener noreferrer" href={link.url}>
