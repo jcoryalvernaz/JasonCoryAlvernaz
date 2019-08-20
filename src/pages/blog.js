1856a4f4b467d9402a9c644a02d90f2399eaaee9
Jason Alvernaz <jason.alvernaz@lcb.state.nv.us>
2019-08-20 10:23:23 -0700

    taking out limit for blog query

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -80,7 +80,6 @@ const BlogPage = ({ data }) => {
 export const pageQuery = graphql`
   query IndexQuery {
     allMarkdownRemark(
-      limit: 10
       filter: { frontmatter: { published: { eq: true } } }
       sort: { fields: frontmatter___date, order: DESC }
     ) {
