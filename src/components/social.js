f05963090a55039bfb7e1508b73dc0fbcd76eb4c
Jason Alvernaz <jason.alvernaz@lcb.state.nv.us>
2019-09-20 10:26:26 -0700

    fixing social links

--- src/components/social.js
+++ src/components/social.js
@@ -23,13 +23,6 @@ const SocialWrapper = styled.div`
   .icon {
     width: 8rem;
     transition: 0.2s all ease-in;
-    :hover {
-      transform: translateY(-10%);
-      filter: ${props =>
-        props.theme.isDark
-          ? `drop-shadow(0px 6px 2px rgba(76, 42, 133, 1))`
-          : `drop-shadow(0px 6px 2px rgba(0, 0, 0, 0.2))`};
-    }
   }
   .arrow {
     fill: ${props =>
@@ -63,6 +56,27 @@ const SocialWrapper = styled.div`
   @media (max-width: 630px) {
     grid-template-columns: 1fr;
     grid-gap: 2rem;
+  }
+  a {
+    position: relative;
+    display: inline-block;
+    :after {
+      content: "";
+      position: absolute;
+      top: 0;
+      right: 0;
+      bottom: 0;
+      left: 0;
+    }
+    :hover {
+      .icon {
+        transform: translateY(-10%);
+        filter: ${props =>
+          props.theme.isDark
+            ? `drop-shadow(0px 6px 2px rgba(76, 42, 133, 1))`
+            : `drop-shadow(0px 6px 2px rgba(0, 0, 0, 0.2))`};
+      }
+    }
   }
 `
 
