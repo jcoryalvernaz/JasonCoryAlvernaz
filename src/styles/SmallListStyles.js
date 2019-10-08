982242f0841798af1e9ae702e420348998d20b1f
Jason Alvernaz <jason.alvernaz@lcb.state.nv.us>
2019-10-08 10:57:16 -0700

    adding hover effect to links

--- src/styles/SmallListStyles.js
+++ src/styles/SmallListStyles.js
@@ -13,6 +13,16 @@ const SmallListStyles = styled.ul`
   );
   li a {
     border-bottom: 0.2rem solid ${props => props.theme.green};
+    background-size: 100% 200%;
+    background-image: linear-gradient(
+      to top,
+      ${props => props.theme.green + "4D"} 50%,
+      transparent 50%
+    );
+    transition: background-position 0.5s;
+    :hover {
+      background-position: 0 100%;
+    }
   }
 `
 
